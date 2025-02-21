a0d44864158722697754a8427e9a140cab3e34de
CBurnham-BBB <cb604482@wne.edu>
2025-02-21 03:20:56 +0000

    Problem 2 finished

--- Problem2/simpleAdmin.sh
+++ Problem2/simpleAdmin.sh
@@ -5,13 +5,20 @@
 #	Creates directory inside above-mentioned directory using ARG2 as name.
 #	Creates a Hello World text file under second-mentioned directory using ARG3 as name.
 #	Creates a softlink from PWD to second-mentioned directory using ARG4 as name.
-#	Displays the actions being taken
+#	Displays the actions being taken as the script is running.
+# Script arguments:
+#	1. Dir1 directory name
+#	2. Dir2 directory name
+#	3. Message text file name
+#	4. Softlink to Dir2 name
+# Script invocation:
+#	Utilize ./simpleAdmin.sh to run the script from the Problem2 directory.
 
 mkdir "$PWD/$1"
-echo Creating directory named $ARG1
+echo Creating directory named $1
 mkdir "$PWD/$1/$2"
-echo Creating directory named $ARG2 under $ARG1
+echo Creating directory named $2 under $1
 echo "Hello World!">"$PWD/$1/$2/$3"
-echo Creating Hello World message under $ARG2 directory
+echo Creating Hello World message under $2 directory
 ln -s "$PWD/$1/$2" "$PWD/$4"
-echo Creating link from PWD to $ARG2
+echo Creating link from PWD to $2
